63daa70374fff7ac5143c4d09ae15d87bbafeb74
Alex <you@example.com>
2017-06-12 18:00:24 +0100

    Added working 2.2.4 support. However the client does not use the new communication protocol that the official client uses.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Runescape 3 NXT launcher (linux)
 
-This project is a minimalistic launcher for Runescape 3 (NXT).
+This project is a minimalistic launcher for Runescape 3 (NXT). Compatible with v2.2.4.
 It uses a (portable, /bin/sh) shell script to download the game config, update files and then run the game. 
 This project has been tested on Debian and Void GNU/Linux.
 
@@ -12,12 +12,11 @@ rhash - rhash - used to verify that files were downloaded correctly
 curl - curl - used to download files
 7zip - p7zip - used to compress/decompress files
 gcc - gcc - compile the program
-xcb, xcb-util - changing window title
 ```
 
 To run on a fresh ubuntu install you should be able to run:
 ```
-sudo apt install rhash curl p7zip-full gcc git libsdl2-2.0-0 libx11-xcb-dev libx11-xcb-util-dev
+sudo apt install rhash curl p7zip-full gcc git libsdl2-2.0-0
 ```
 and then follow the instructions below.
 
@@ -26,7 +25,7 @@ and then follow the instructions below.
 ```
 git clone https://github.com/yoloium/rs3-launcher.git
 cd rs3-launcher
-gcc launcher.c -o launcher -ldl -lxcb -lpthread
+gcc launcher.c -o launcher -lpthread
 ```
 DO NOT RUN THE CLIENT YET
 Open rs3-launcher in your favourite text editor e.g.
@@ -59,7 +58,7 @@ NOTE if you have an older graphics card, you can replace '3.0' with '2.1' so tha
 Will consider accepting pull requests. But don't expect me to look here. PM me in game or something to let me know; RSN = yoloium
 
 ## Alternatives
-1. Python + shell equivalent: https://gist.github.com/yoloium/3b2ece56c33cf6dc013e75cc64e4124a.
+1. Python + shell equivalent: https://gist.github.com/yoloium/3b2ece56c33cf6dc013e75cc64e4124a. (not updated for 2.2.4)
 2. Minimal (legacy) java client launcher: https://gist.github.com/yoloium/0dd28b23da8cf1d9c409972141137967. NOTE: read comment for RS3 usage.
 3. (bloated) C++ equivalent: https://github.com/syldrathecat/nxtlauncher
 
--- launcher.c
+++ launcher.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
 #include <dlfcn.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <stdint.h>
 #include <endian.h>
 #include <unistd.h>
-#include <xcb/xcb.h>
-#include <xcb/xcb_atom.h>
 #include <limits.h>
+#include <pthread.h>
 
-#define WINDOW_TITLE "RuneScape"
 #define FIFO_PREFIX "/tmp/RS2LauncherConnection_"
 
 typedef struct {
@@ -30,26 +28,24 @@ void print_message(const char *prefix, const char *msg, int length){
 	printf("] %iB\n", length-2);
 }
 
-int handle_message(const char *msg, int length, int fd_out, char *cache_folder, char *user_folder, char* pid){
+int handle_message(const char *msg, int length, int fd_out, char *cache_folder, char *user_folder){
 	char msg_id[2] = {msg[0], msg[1]};
 	uint16_t id = be16toh(*(uint16_t *) msg_id);
 	print_message("GOT: ", msg, length);
 	if(id == 0) {
 		// calculate reply size to make buffer
-		uint16_t reply_size = 17 + strlen(cache_folder) + strlen(user_folder);
+		uint16_t reply_size = htole16(17 + strlen(cache_folder) + strlen(user_folder));
 		char reply[reply_size];
 		
 		// declare variables to write in reply
 		uint16_t msg_id = htobe16(1);
 		char *bytes1 = "\x00\x02\x00\x00\x00\x00";
-		uint32_t parent_id = 0;
 		char *bytes2 = "\x00\x03\x00";
 		
 		// zero and fill reply. 
                 memset(reply, 0, reply_size); 
 		memcpy(reply, &msg_id, 2);
 		memcpy(reply+2, bytes1, 6);
-		memcpy(reply+8, &parent_id, 4);
 		int clen = strlen(cache_folder), ulen = strlen(user_folder);
 		memcpy(reply+12, cache_folder, clen);
 		memcpy(reply+13+clen, user_folder, ulen);
@@ -61,35 +57,12 @@ int handle_message(const char *msg, int length, int fd_out, char *cache_folder, 
 		write(fd_out, reply, reply_size);
 		print_message("SENT: ", reply, reply_size);			
 
-	} else if (id == 2 && length == 12){ 
-		// connect to display
-		xcb_connection_t *connection = xcb_connect(NULL, NULL);
-		// only continue if successfully connected
-		if(connection){
-			// extract and convert window id
-	                char wid[4] = {msg[8], msg[9], msg[10], msg[11]};
-			uint32_t window_id = be32toh(* (uint32_t *) wid);
-			// create window title
-			char win_title[strlen(WINDOW_TITLE) + sizeof(pid) + 1];
-			sprintf(win_title, "%s %s", WINDOW_TITLE, pid);
-			// set window title
-			xcb_change_property(connection, 
-				XCB_PROP_MODE_REPLACE, 
-				(xcb_window_t) window_id, 
-				XCB_ATOM_WM_NAME, 
-				XCB_ATOM_STRING, 
-				8,
-				strlen(win_title),
-				(const unsigned char*) win_title
-			);
-			xcb_flush(connection);
-			xcb_disconnect(connection);
-		}
-	} 
+	} else if (id == 0x0020){
+		write(fd_out, "\4\0\0\v\0\1" ,6);
+	}
 }
 
 void *handle_ipc(void *args_ptr){
-
 	ipc_args *args = args_ptr;
 	
 	// Size and assemble FIFO locations
@@ -126,7 +99,7 @@ void *handle_ipc(void *args_ptr){
 		}
 
 		// handle message
-		handle_message(msg, msg_len, fd_out, args->cache_f, args->user_f, args->pid);
+		handle_message(msg, msg_len, fd_out, args->cache_f, args->user_f);
 	}
 	printf("Ended communication with client.\n");
 
@@ -138,44 +111,30 @@ void *handle_ipc(void *args_ptr){
 	unlink(in_fifo);
 	unlink(out_fifo);
 }
-
 /*
 	launch parameters:
-	0=executable name 1=process id
-	2=cache folder  3=user folder
-	4=args
+	0=executable name 
+	1=cache folder  2=user folder
+	3=args n-1=pid
 */ 
 int main(int argc, char *argv[]){
-	if(argc != 5){
-		printf("Incorrect number of arguments recieved.\n");
-		exit(1);
-	}
-
-	// load lib
-	void *lib = dlopen("./librs2client.so", RTLD_NOW);
-	if(!lib){
-		printf("Error loading lib.\n");
-		exit(2);
-	}
+	extern char** environ;
 
 	// create thread params
-	ipc_args thread_args = { argv[1], argv[2], argv[3] };
+	ipc_args thread_args = { argv[argc-1], argv[1], argv[2] };
 
 	// create and run IPC thread.
 	pthread_t thread;
 	pthread_create(&thread, NULL, handle_ipc, &thread_args);
 	
-	// Get address of method, checking that it was successfully able to find function.
-	// int RunMainBootstrap(const char* params, int width, int height);
-	int (*rmbs)(const char*, int, int) = dlsym(lib, "RunMainBootstrap");
-
-	// print error or run function
-	if (dlerror() != NULL){
-		printf("Unable to find bootstrap function.\n");
+	int result;
+	pid_t pid = fork();
+	if(pid > 0){
+		waitpid(pid, &result, 0);
 	} else {
-		rmbs(argv[4], 1024, 768);
+		execve(argv[3], argv+3, environ);
 	}
-	// cleanup
+
 	pthread_join(thread, NULL);
-	dlclose(lib);
+	return result;
 }
